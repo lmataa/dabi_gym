8e6f6950fe29add50e568a7699b3fc146bf53ffb
Luis Mata <luismata06@gmail.com>
2021-12-25 11:57:30 +0100

    repo reorg and clean up of some files

--- random_action.py
+++ random_action.py
@@ -11,22 +11,29 @@ def main(raw_args=None):
     # env = gym.make('Assault-v0')
     # env = gym.make('Breakout-ram-v0')
     # env = gym.make('Freeway-v0')
-    env = gym.make('SpaceInvaders-v4')
+    # env = gym.make('SpaceInvaders-v4')
+    env = gym.make('LunarLander-v2')
     # env = gym.make('Pong-v4')
     print(env.action_space)
     print(env.observation_space.shape)
-    print()
+    scores = []
     for e in range(20):
         observation = env.reset()
+        score = 0
         for t in range(1000):
             env.render()
-            action = np.random.choice([0,1,2,3,4,5]) #env.action_space.sample()
+            action = np.random.choice(range(env.action_space.n)) #env.action_space.sample()
             observation, reward, done, info = env.step(action)
+            score += reward
             if done:
-                print(f"Episode {e} finished after {t+1} timesteps")
                 break
             if raw_args.debug and t%5==0:
                 input("Press Enter to continue...")
+        scores.append(score)
+        avg_score = np.mean(scores[-100:])
+        print('episode ', e, 'score %.2f' % score,
+                'average score %.2f' % avg_score,
+                'finished after: %d steps' % t)
     env.close()
 
 if __name__ == "__main__":
